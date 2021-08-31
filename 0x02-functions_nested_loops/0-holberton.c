36d0f074c4dcc3ac72b95fe3b5b96d619e3ba9d3
douglasmcqoy <douglasmcqoy@gmail.com>
2021-08-31 10:37:08 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-char str[] = "Holberton\n";
-int i = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (str[i] != '\0')
-		{
-		char c = str[i];
-
-		_putchar(c);
-		i++;
-		}
 	return (0);
 }
