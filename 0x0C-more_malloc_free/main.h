03c4143e63918c7ef92ade2caec2f7b97959b0cb
douglasmcqoy@gmail.com <douglasmcqoy@outlook.com>
2021-09-24 20:27:31 +0000

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /**
- * Auth: Brennan D Baraban
+ * Auth: Douglas Quaye
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0B-more_malloc_free directory.
  */
